d96f3c18a112d3472c522f1d545e19b2c9e61773
PRO X <bredalisgautreaux@gmail>
2024-12-08 10:48:47 +0100

    refactor: 🌳 Programas refactorizados

--- static/JS/main.js
+++ static/JS/main.js
@@ -1,10 +1,12 @@
 
 var clave = document.getElementById("contraseña");
 
+// Mostrar la contraseña
 clave.addEventListener("focusin", () => {
 	clave.type = "text";
 });
 
+// No mostrar la contraseña
 clave.addEventListener("focusout", () => {
 	clave.type = "password";
 });
